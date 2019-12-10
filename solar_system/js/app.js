300c67ba46b888bbd72330dbe647ecf859b90296
jujoiner <54684212+jujoiner@users.noreply.github.com>
2019-12-10 14:41:00 -0500

    Update app.js

--- solar_system/js/app.js
+++ solar_system/js/app.js
@@ -10,7 +10,7 @@ var scene = createScene();
 
 //Global Variables for the planets
 var sun;
-var planet_1;
+var H_028;
 
 //Setting up the scene
 function createScene() {
@@ -21,7 +21,7 @@ function createScene() {
     "Camera",
     Math.PI / 2,
     Math.PI / 4,
-    20,
+    35,
     BABYLON.Vector3.Zero(),
     scene
   );
@@ -44,156 +44,156 @@ function createScene() {
   sun.material.emissiveColor = new BABYLON.Color3(0.8, 0.4, 0.4);
 
   // --- Planet One ---
-  planet_1 = BABYLON.MeshBuilder.CreateSphere(
+  H_028 = BABYLON.MeshBuilder.CreateSphere(
     "sphere",
     { diameter: 0.3 },
     scene
   );
-  planet_1.position.z = 1.75;
+  H_028.position.z = 1.75;
 
-  var planetMaterial = new BABYLON.StandardMaterial("planetSurface", scene);
-  planetMaterial.emissiveTexture = new BABYLON.Texture("textures/h-028.jpeg", scene);
-  planet_1.material = planetMaterial;
+  var H_028Material = new BABYLON.StandardMaterial("planetSurface", scene);
+  H_028Material.emissiveTexture = new BABYLON.Texture("textures/h-028.jpeg", scene);
+  H_028.material = H_028Material;
   
-  planet_1.position.x = 5;
-	planet_1.bakeCurrentTransformIntoVertices();
-  planet_1.position.x = 0;
+  H_028.position.x = 5;
+	H_028.bakeCurrentTransformIntoVertices();
+  H_028.position.x = 0;
   
   // --- Planet Two ---
-  planet_2 = BABYLON.MeshBuilder.CreateSphere(
+  Asiaq = BABYLON.MeshBuilder.CreateSphere(
     "sphere",
     { diameter: 3 },
     scene
   );
-  planet_2.position.x = 5.75;
+  Asiaq.position.x = 5.75;
 
-  var planet2Material = new BABYLON.StandardMaterial("planetSurface", scene);
-  planet2Material.emissiveTexture = new BABYLON.Texture("textures/Asiaq.jpeg", scene);
-  planet_2.material = planet2Material;
+  var AsiaqMaterial = new BABYLON.StandardMaterial("planetSurface", scene);
+  AsiaqMaterial.emissiveTexture = new BABYLON.Texture("textures/Asiaq.jpeg", scene);
+  Asiaq.material = AsiaqMaterial;
 
 
-  planet_2.position.x = 10;
-	planet_2.bakeCurrentTransformIntoVertices();
-  planet_2.position.x = 0;
+  Asiaq.position.x = 10;
+	Asiaq.bakeCurrentTransformIntoVertices();
+  Asiaq.position.x = 0;
   
   // --- Planet Three ---
-  planet_3 = BABYLON.MeshBuilder.CreateSphere(
+  Efaja = BABYLON.MeshBuilder.CreateSphere(
     "sphere",
     { diameter: 0.7 },
     scene
   );
-  planet_3.position.y = 5.75;
+  Efaja.position.y = 5.75;
 
-  var planet3Material = new BABYLON.StandardMaterial("planetSurface", scene);
-  planet3Material.emissiveTexture = new BABYLON.Texture("textures/Efaja.jpeg", scene);
-  planet_3.material = planet3Material;
+  var EfajaMaterial = new BABYLON.StandardMaterial("planetSurface", scene);
+  EfajaMaterial.emissiveTexture = new BABYLON.Texture("textures/Efaja.jpeg", scene);
+  Efaja.material = EfajaMaterial;
 
-  planet_3.position.x = 5;
-	planet_3.bakeCurrentTransformIntoVertices();
-  planet_3.position.x = 0;
+  Efaja.position.x = 5;
+	Efaja.bakeCurrentTransformIntoVertices();
+  Efaja.position.x = 0;
   
   // --- Planet Four ---
-  planet_4 = BABYLON.MeshBuilder.CreateSphere(
+  Grill = BABYLON.MeshBuilder.CreateSphere(
     "sphere",
     { diameter: 1.5 },
     scene
   );
-  planet_4.position.y = 10;
+  Grill.position.y = 10;
 
-  var planet4Material = new BABYLON.StandardMaterial("planetSurface", scene);
-  planet4Material.emissiveTexture = new BABYLON.Texture("textures/Efaja.jpeg", scene);
-  planet_4.material = planet4Material;
+  var GrillMaterial = new BABYLON.StandardMaterial("planetSurface", scene);
+  GrillMaterial.emissiveTexture = new BABYLON.Texture("textures/Grill.jpeg", scene);
+  Grill.material = GrillMaterial;
 
-  planet_4.position.x = 5.7;
-  planet_4.bakeCurrentTransformIntoVertices();
-  planet_4.position.x = 0;
+  Grill.position.x = 5.7;
+  Grill.bakeCurrentTransformIntoVertices();
+  Grill.position.x = 0;
       
   // --- Planet Five ---
-  planet_5 = BABYLON.MeshBuilder.CreateSphere(
+  Habitat_7 = BABYLON.MeshBuilder.CreateSphere(
     "sphere",
     { diameter: 0.65 },
     scene
   );
-  planet_5.position.z = 7.3;
+  Habitat_7.position.z = 7.3;
 
-  var planet5Material = new BABYLON.StandardMaterial("planetSurface", scene);
-  planet5Material.emissiveTexture = new BABYLON.Texture("textures/Efaja.jpeg", scene);
-  planet_5.material = planet5Material;
+  var Habitat7Material = new BABYLON.StandardMaterial("planetSurface", scene);
+  Habitat7Material.emissiveTexture = new BABYLON.Texture("textures/Habitat_7.jpeg", scene);
+  Habitat_7.material = Habitat7Material;
 
-  planet_5.position.x = 7.3;
-  planet_5.bakeCurrentTransformIntoVertices();
-  planet_5.position.x = 0;
+  Habitat_7.position.x = 7.3;
+  Habitat_7.bakeCurrentTransformIntoVertices();
+  Habitat_7.position.x = 0;
 
   // --- Planet Six ---
-  planet_6 = BABYLON.MeshBuilder.CreateSphere(
+  Yhraen = BABYLON.MeshBuilder.CreateSphere(
     "sphere",
     { diameter: 0.88 },
     scene
   );
-  planet_6.position.x = 9.65;
+  Yhraen.position.x = 9.65;
 
-  var planet6Material = new BABYLON.StandardMaterial("planetSurface", scene);
-  planet6Material.emissiveTexture = new BABYLON.Texture("textures/Efaja.jpeg", scene);
-  planet_6.material = planet6Material;
+  var YhraenMaterial = new BABYLON.StandardMaterial("planetSurface", scene);
+  YhraenMaterial.emissiveTexture = new BABYLON.Texture("textures/Yhraen.jpeg", scene);
+  Yhraen.material = YhraenMaterial;
 
-  planet_6.position.x = 9.65;
-  planet_6.bakeCurrentTransformIntoVertices();
-  planet_6.position.x = 0;
+  Yhraen.position.x = 9.65;
+  Yhraen.bakeCurrentTransformIntoVertices();
+  Yhraen.position.x = 0;
   
   // --- Rings ---
   var myPlane = BABYLON.MeshBuilder.CreateCylinder("myPlane", {diameter:5 , height: .050}, scene);
   myPlane.position.x = 10;
-  planet_2.addChild(myPlane);
+  Asiaq.addChild(myPlane);
 
   var myRing = BABYLON.MeshBuilder.CreateCylinder("myPlane", {diameter:5 , height: .050}, scene);
   myRing.position.y = 10;
   myRing.position.x = 5.7;
-  planet_4.addChild(myRing);
+  Grill.addChild(myRing);
 
   var myCircle = BABYLON.MeshBuilder.CreateCylinder("myPlane", {diameter:5 , height: .050}, scene);
   myCircle.position.x = 7.3;
   myCircle.position.z = 7.3;
-  planet_5.addChild(myCircle);
+  Habitat_7.addChild(myCircle);
 
   return scene;
 }
 
 //Starting the render loop
 engine.runRenderLoop(function() {
-  orbit();
+ orbit();
   scene.render();
 });
 
 //Orbit function for da planets
 function orbit() {
-  TweenLite.to(planet_1.rotation, 1, {
+  TweenLite.to(H_028.rotation, 1, {
     y: "+=.5",
     x: "+=.5"
   });
   
-  TweenLite.to(planet_2.rotation, 1, {
+  TweenLite.to(Asiaq.rotation, 1, {
     y: "+=.3",
-    x: "+=.01"
+    x: "+=.31"
   });
   
-  TweenLite.to(planet_3.rotation, 1, {
+  TweenLite.to(Efaja.rotation, 1, {
     y: "+=.75",
-    x: "+=.5"
+    x: "+=.75"
   });
 
-  TweenLite.to(planet_4.rotation, 1, {
-    y: "+=.406",
+  TweenLite.to(Grill.rotation, 1, {
+    y: "+=.706",
     x: "+=.709"
   });
 
-  TweenLite.to(planet_5.rotation, 1, {
+  TweenLite.to(Habitat_7.rotation, 1, {
     y: "+=.251",
-    x: "+=.586"
+    x: "+=.286"
   });
 
-  TweenLite.to(planet_6.rotation, 1, {
+  TweenLite.to(Yhraen.rotation, 1, {
     y: "+=.195",
-    x: "+=.055"
+    x: "+=.155"
   });
   //console.log(planet_1.rotation.x);
 }
@@ -211,8 +211,8 @@ sun.actionManager.registerAction(
 );
 
 //------------------------------------------------------------------------- planet 1
-planet_1.actionManager = new BABYLON.ActionManager(scene);
-planet_1.actionManager.registerAction(
+H_028.actionManager = new BABYLON.ActionManager(scene);
+H_028.actionManager.registerAction(
   new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function(
     event
   ) {
@@ -223,8 +223,8 @@ planet_1.actionManager.registerAction(
 );
 
 //------------------------------------------------------------------------- planet 2
-planet_2.actionManager = new BABYLON.ActionManager(scene);
-planet_2.actionManager.registerAction(
+Asiaq.actionManager = new BABYLON.ActionManager(scene);
+Asiaq.actionManager.registerAction(
   new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function(
     event
   ) {
