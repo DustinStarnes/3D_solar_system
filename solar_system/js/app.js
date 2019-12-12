2a6a127e364374c783e975d18933ce2c506a039b
Dustin Starnes <dustin.starnes17@gmail.com>
2019-12-12 13:49:44 -0500

    updated camera

--- solar_system/js/app.js
+++ solar_system/js/app.js
@@ -6,7 +6,7 @@ var engine = new BABYLON.Engine(canvas, true);
 var camera;
 
 var scene = createScene();
-  scene.clearColor = new BABYLON.Color3(0.1, 0.007, 0.015);
+scene.clearColor = new BABYLON.Color3(0.1, 0.007, 0.015);
 
 //Global Variables for the planets
 var sun;
@@ -17,15 +17,11 @@ function createScene() {
   var scene = new BABYLON.Scene(engine);
 
   //Add a camera to the scene and attach it to the canvas
-  camera = new BABYLON.ArcRotateCamera(
-    "Camera",
-    Math.PI / 2,
-    Math.PI / 4,
-    35,
-    BABYLON.Vector3.Zero(),
+  camera = new BABYLON.UniversalCamera(
+    "UC",
+    new BABYLON.Vector3(0, 0, -25),
     scene
   );
-  camera.attachControl(canvas, true);
 
   //Add lights to the scene
   var myLight = new BABYLON.PointLight(
@@ -44,70 +40,65 @@ function createScene() {
   sun.material.emissiveColor = new BABYLON.Color3(0.8, 0.4, 0.4);
 
   // --- Planet One ---
-  H_028 = BABYLON.MeshBuilder.CreateSphere(
-    "sphere",
-    { diameter: 0.3 },
-    scene
-  );
+  H_028 = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 0.3 }, scene);
   H_028.position.z = 1.75;
 
   var H_028Material = new BABYLON.StandardMaterial("planetSurface", scene);
-  H_028Material.emissiveTexture = new BABYLON.Texture("textures/h-028.jpeg", scene);
-  H_028.material = H_028Material;
-  
-  H_028.position.x = 5;
-	H_028.bakeCurrentTransformIntoVertices();
-  H_028.position.x = 0;
-  
-  // --- Planet Two ---
-  Asiaq = BABYLON.MeshBuilder.CreateSphere(
-    "sphere",
-    { diameter: 3 },
+  H_028Material.emissiveTexture = new BABYLON.Texture(
+    "textures/h-028.jpeg",
     scene
   );
+  H_028.material = H_028Material;
+
+  H_028.position.x = 5;
+  H_028.bakeCurrentTransformIntoVertices();
+  H_028.position.x = 0;
+
+  // --- Planet Two ---
+  Asiaq = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 3 }, scene);
   Asiaq.position.x = 5.75;
 
   var AsiaqMaterial = new BABYLON.StandardMaterial("planetSurface", scene);
-  AsiaqMaterial.emissiveTexture = new BABYLON.Texture("textures/Asiaq.jpeg", scene);
-  Asiaq.material = AsiaqMaterial;
-
-
-  Asiaq.position.x = 10;
-	Asiaq.bakeCurrentTransformIntoVertices();
-  Asiaq.position.x = 0;
-  
-  // --- Planet Three ---
-  Efaja = BABYLON.MeshBuilder.CreateSphere(
-    "sphere",
-    { diameter: 0.7 },
+  AsiaqMaterial.emissiveTexture = new BABYLON.Texture(
+    "textures/Asiaq.jpeg",
     scene
   );
+  Asiaq.material = AsiaqMaterial;
+
+  Asiaq.position.x = 10;
+  Asiaq.bakeCurrentTransformIntoVertices();
+  Asiaq.position.x = 0;
+
+  // --- Planet Three ---
+  Efaja = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 0.7 }, scene);
   Efaja.position.y = 5.75;
 
   var EfajaMaterial = new BABYLON.StandardMaterial("planetSurface", scene);
-  EfajaMaterial.emissiveTexture = new BABYLON.Texture("textures/Efaja.jpeg", scene);
+  EfajaMaterial.emissiveTexture = new BABYLON.Texture(
+    "textures/Efaja.jpeg",
+    scene
+  );
   Efaja.material = EfajaMaterial;
 
   Efaja.position.x = 5;
-	Efaja.bakeCurrentTransformIntoVertices();
+  Efaja.bakeCurrentTransformIntoVertices();
   Efaja.position.x = 0;
-  
+
   // --- Planet Four ---
-  Grill = BABYLON.MeshBuilder.CreateSphere(
-    "sphere",
-    { diameter: 1.5 },
-    scene
-  );
+  Grill = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 1.5 }, scene);
   Grill.position.y = 10;
 
   var GrillMaterial = new BABYLON.StandardMaterial("planetSurface", scene);
-  GrillMaterial.emissiveTexture = new BABYLON.Texture("textures/Grill.jpeg", scene);
+  GrillMaterial.emissiveTexture = new BABYLON.Texture(
+    "textures/Grill.jpeg",
+    scene
+  );
   Grill.material = GrillMaterial;
 
   Grill.position.x = 5.7;
   Grill.bakeCurrentTransformIntoVertices();
   Grill.position.x = 0;
-      
+
   // --- Planet Five ---
   Habitat_7 = BABYLON.MeshBuilder.CreateSphere(
     "sphere",
@@ -117,7 +108,10 @@ function createScene() {
   Habitat_7.position.z = 7.3;
 
   var Habitat7Material = new BABYLON.StandardMaterial("planetSurface", scene);
-  Habitat7Material.emissiveTexture = new BABYLON.Texture("textures/Habitat_7.jpeg", scene);
+  Habitat7Material.emissiveTexture = new BABYLON.Texture(
+    "textures/Habitat_7.jpeg",
+    scene
+  );
   Habitat_7.material = Habitat7Material;
 
   Habitat_7.position.x = 7.3;
@@ -133,24 +127,39 @@ function createScene() {
   Yhraen.position.x = 9.65;
 
   var YhraenMaterial = new BABYLON.StandardMaterial("planetSurface", scene);
-  YhraenMaterial.emissiveTexture = new BABYLON.Texture("textures/Yhraen.jpeg", scene);
+  YhraenMaterial.emissiveTexture = new BABYLON.Texture(
+    "textures/Yhraen.jpeg",
+    scene
+  );
   Yhraen.material = YhraenMaterial;
 
   Yhraen.position.x = 9.65;
   Yhraen.bakeCurrentTransformIntoVertices();
   Yhraen.position.x = 0;
-  
+
   // --- Rings ---
-  var myPlane = BABYLON.MeshBuilder.CreateCylinder("myPlane", {diameter:5 , height: .050}, scene);
+  var myPlane = BABYLON.MeshBuilder.CreateCylinder(
+    "myPlane",
+    { diameter: 5, height: 0.05 },
+    scene
+  );
   myPlane.position.x = 10;
   Asiaq.addChild(myPlane);
 
-  var myRing = BABYLON.MeshBuilder.CreateCylinder("myPlane", {diameter:5 , height: .050}, scene);
+  var myRing = BABYLON.MeshBuilder.CreateCylinder(
+    "myPlane",
+    { diameter: 5, height: 0.05 },
+    scene
+  );
   myRing.position.y = 10;
   myRing.position.x = 5.7;
   Grill.addChild(myRing);
 
-  var myCircle = BABYLON.MeshBuilder.CreateCylinder("myPlane", {diameter:5 , height: .050}, scene);
+  var myCircle = BABYLON.MeshBuilder.CreateCylinder(
+    "myPlane",
+    { diameter: 5, height: 0.05 },
+    scene
+  );
   myCircle.position.x = 7.3;
   myCircle.position.z = 7.3;
   Habitat_7.addChild(myCircle);
@@ -160,7 +169,7 @@ function createScene() {
 
 //Starting the render loop
 engine.runRenderLoop(function() {
- orbit();
+  orbit();
   scene.render();
 });
 
@@ -170,12 +179,12 @@ function orbit() {
     y: "+=.5",
     x: "+=.5"
   });
-  
+
   TweenLite.to(Asiaq.rotation, 1, {
     y: "+=.3",
     x: "+=.31"
   });
-  
+
   TweenLite.to(Efaja.rotation, 1, {
     y: "+=.75",
     x: "+=.75"
